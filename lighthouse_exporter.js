138b2f4e48914b5b48dd3d4b2d192abfa47f39a8
Philipp Keschl <pkesc7qm@gmail.com>
2019-06-01 16:25:27 +0200

    add audit timings

--- lighthouse_exporter.js
+++ lighthouse_exporter.js
@@ -27,21 +27,33 @@ http.createServer(async (req, res) => {
 
         data.push('# HELP lighthouse_exporter_info Exporter Info');
         data.push('# TYPE lighthouse_exporter_info gauge');
-        data.push(`lighthouse_exporter_info{version="0.1.1",chrome_version="${await browser.version()}"} 1`);
-
-        data.push('# HELP lighthouse_score The Score per Category');
-        data.push('# TYPE lighthouse_score gauge');
+        data.push(`lighthouse_exporter_info{version="0.2.0",chrome_version="${await browser.version()}",node_version="${process.version}"} 1`);
 
         await lighthouse(target, {
             port: url.parse(browser.wsEndpoint()).port,
             output: 'json'
         })
             .then(results => {
+                data.push('# HELP lighthouse_score The Score per Category');
+                data.push('# TYPE lighthouse_score gauge');
+
                 for(var category in results.lhr.categories){
                     var item = results.lhr.categories[category];
 
                     data.push(`lighthouse_score{category="${category}"} ${item.score * 100}`);
                 }
+
+                var audits = results.lhr.audits;
+
+                data.push('# HELP lighthouse_timings Audit timings in ms');
+                data.push('# TYPE lighthouse_timings gauge');
+
+                data.push(`lighthouse_timings{audit="first-contentful-paint"} ${Math.round(audits["first-contentful-paint"].numericValue)}`);
+                data.push(`lighthouse_timings{audit="first-meaningful-paint"} ${Math.round(audits["first-meaningful-paint"].numericValue)}`);
+                data.push(`lighthouse_timings{audit="speed-index"} ${Math.round(audits["speed-index"].numericValue)}`);
+                data.push(`lighthouse_timings{audit="first-cpu-idle"} ${Math.round(audits["first-cpu-idle"].numericValue)}`);
+                data.push(`lighthouse_timings{audit="interactive"} ${Math.round(audits["interactive"].numericValue)}`);
+                data.push(`lighthouse_timings{audit="estimated-input-latency"} ${Math.round(audits["estimated-input-latency"].numericValue)}`);
             });
 
         await browser.close();
