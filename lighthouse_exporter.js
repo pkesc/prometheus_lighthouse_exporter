0b88430c1b17438b273e3f4cf8cf6447089398d7
Philipp Keschl <pkesc7qm@gmail.com>
2019-08-10 15:15:30 +0200

    add better error handling because i experience some deadlocks

--- lighthouse_exporter.js
+++ lighthouse_exporter.js
@@ -28,43 +28,47 @@ http.createServer(async (req, res) => {
         var target = q.query.target;
         var data = [];
 
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        try{
+            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
-        data.push('# HELP lighthouse_exporter_info Exporter Info');
-        data.push('# TYPE lighthouse_exporter_info gauge');
-        data.push(`lighthouse_exporter_info{version="0.2.3",chrome_version="${await browser.version()}",node_version="${process.version}"} 1`);
+            data.push('# HELP lighthouse_exporter_info Exporter Info');
+            data.push('# TYPE lighthouse_exporter_info gauge');
+            data.push(`lighthouse_exporter_info{version="0.2.4",chrome_version="${await browser.version()}",node_version="${process.version}"} 1`);
 
-        await lighthouse(target, {
-            port: url.parse(browser.wsEndpoint()).port,
-            output: 'json'
-        })
-            .then(results => {
-                data.push('# HELP lighthouse_score The Score per Category');
-                data.push('# TYPE lighthouse_score gauge');
-
-                for(var category in results.lhr.categories){
-                    var item = results.lhr.categories[category];
-
-                    data.push(`lighthouse_score{category="${category}"} ${item.score * 100}`);
-                }
-
-                var audits = results.lhr.audits;
-
-                data.push('# HELP lighthouse_timings Audit timings in ms');
-                data.push('# TYPE lighthouse_timings gauge');
-
-                data.push(`lighthouse_timings{audit="first-contentful-paint"} ${Math.round(audits["first-contentful-paint"].numericValue)}`);
-                data.push(`lighthouse_timings{audit="first-meaningful-paint"} ${Math.round(audits["first-meaningful-paint"].numericValue)}`);
-                data.push(`lighthouse_timings{audit="speed-index"} ${Math.round(audits["speed-index"].numericValue)}`);
-                data.push(`lighthouse_timings{audit="first-cpu-idle"} ${Math.round(audits["first-cpu-idle"].numericValue)}`);
-                data.push(`lighthouse_timings{audit="interactive"} ${Math.round(audits["interactive"].numericValue)}`);
-                data.push(`lighthouse_timings{audit="estimated-input-latency"} ${Math.round(audits["estimated-input-latency"].numericValue)}`);
+            await lighthouse(target, {
+                port: url.parse(browser.wsEndpoint()).port,
+                output: 'json'
             })
-            .catch(error => {
-                console.error(error);
-            });
+                .then(results => {
+                    data.push('# HELP lighthouse_score The Score per Category');
+                    data.push('# TYPE lighthouse_score gauge');
 
-        await browser.close();
+                    for(var category in results.lhr.categories){
+                        var item = results.lhr.categories[category];
+
+                        data.push(`lighthouse_score{category="${category}"} ${item.score * 100}`);
+                    }
+
+                    var audits = results.lhr.audits;
+
+                    data.push('# HELP lighthouse_timings Audit timings in ms');
+                    data.push('# TYPE lighthouse_timings gauge');
+
+                    data.push(`lighthouse_timings{audit="first-contentful-paint"} ${Math.round(audits["first-contentful-paint"].numericValue)}`);
+                    data.push(`lighthouse_timings{audit="first-meaningful-paint"} ${Math.round(audits["first-meaningful-paint"].numericValue)}`);
+                    data.push(`lighthouse_timings{audit="speed-index"} ${Math.round(audits["speed-index"].numericValue)}`);
+                    data.push(`lighthouse_timings{audit="first-cpu-idle"} ${Math.round(audits["first-cpu-idle"].numericValue)}`);
+                    data.push(`lighthouse_timings{audit="interactive"} ${Math.round(audits["interactive"].numericValue)}`);
+                    data.push(`lighthouse_timings{audit="estimated-input-latency"} ${Math.round(audits["estimated-input-latency"].numericValue)}`);
+                })
+                .catch(error => {
+                    console.error("Lighthouse", Date(), error);
+                });
+
+            await browser.close();
+        } catch(error) {
+            console.error("Generic", Date(), error);
+        }
 
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write(data.join("\n"));
